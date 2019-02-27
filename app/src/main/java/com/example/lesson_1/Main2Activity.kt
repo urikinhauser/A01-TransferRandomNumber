30b7238bde2ab7a09ffca5a6197bca35033af505
Urik Inhauser <urikhein@gmail.com>
2019-02-27 16:40:57 -0300

    First Commit

--- app/src/main/java/com/example/lesson_1/Main2Activity.kt
+++ app/src/main/java/com/example/lesson_1/Main2Activity.kt
@@ -2,19 +2,25 @@ package com.example.lesson_1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2.*
+import kotlin.random.Random
 
 class Main2Activity : AppCompatActivity() {
 
-    //Random value for the first activity
-    val rand1 =  Random.nextInt(0, 10)
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        //Action to call second activity with the random number input to the textview1
-        btActivityB.setOnClickListener()
+        //Random value for the first activity
+        val rand2 =  Random.nextInt(0, 10)
+        //Associate First Number(tvNumber1) to the Variable (rand2)
+       // tvNumber1
+        //Action to call second activity with the random number input to the textview1( tvNumber1 ) by clicking Button2(btActivityB)
+       // btActivityB.setOnClickListener{}
         //test
     }
 }
--- app/src/main/java/com/example/lesson_1/MainActivity.kt
+++ app/src/main/java/com/example/lesson_1/MainActivity.kt
@@ -1,25 +1,31 @@
 package com.example.lesson_1
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.R
-import android.widget.EditText
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
 
-    //Random value for the second activity
-    val rand2 =  Random.nextInt(0, 10)
-
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Random value for the second activity
+        val rand1 =  Random.nextInt(0, 10)
+        //Associate Second Number(tvNumber2) to the Variable (rand1)
+        tvNumber1.text = String.valueOf(rand1)
+
+
         //Action to call second activity with the random number input to the textview2
-        btActivityA.setOnClickListener()
-    }
+       // btActivityA.setOnClickListener {
+            // Action
+           // tvNumber1.setText(rand1)
+       //}
 
 
-}
+    }}
