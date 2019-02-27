1ba6f3809c339633097598180071a8254bbdee06
Urik Inhauser <urikhein@gmail.com>
2019-02-27 17:59:27 -0300

    Resolved. First Lesson Done.

--- app/src/main/java/com/example/lesson_1/Main2Activity.kt
+++ app/src/main/java/com/example/lesson_1/Main2Activity.kt
@@ -1,15 +1,12 @@
 package com.example.lesson_1
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2.*
 import kotlin.random.Random
 
 class Main2Activity : AppCompatActivity() {
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +14,15 @@ class Main2Activity : AppCompatActivity() {
 
         //Random value for the first activity
         val rand2 =  Random.nextInt(0, 10)
-        //Associate First Number(tvNumber1) to the Variable (rand2)
-       // tvNumber1
+        //Associate First Number(tvNumber1) to the Variable (rand2) Using .toString
+
         //Action to call second activity with the random number input to the textview1( tvNumber1 ) by clicking Button2(btActivityB)
-       // btActivityB.setOnClickListener{}
+        btActivityB.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("Key2", rand2.toString())
+            startActivity(intent)
+        }
         //test
+        tvNumber2.text = intent.getStringExtra("Key1")
     }
 }
--- app/src/main/java/com/example/lesson_1/MainActivity.kt
+++ app/src/main/java/com/example/lesson_1/MainActivity.kt
@@ -1,34 +1,30 @@
 package com.example.lesson_1
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main2.*
 import kotlin.random.Random
-
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Random value for the second activity
-        val rand1 =  Random.nextInt(0, 10)
+        val rand1 =  Random.nextInt(1, 10)
         //Associate Second Number(tvNumber2) to the Variable (rand1)
-//        tvNumber2.text = String.valueOf(rand1)
-
-        //Testing the Val into textView
-        tvNumber1.text = String.valueOf(rand1)
 
 
         //Action to call second activity with the random number input to the textview2
-       // btActivityA.setOnClickListener {
+        btActivityA.setOnClickListener {
             // Action
-           // tvNumber1.setText(rand1)
-       //}
-
+            //tvNumber2.text = rand1.toString()
+            val intent = Intent(this, Main2Activity::class.java)
+            intent.putExtra("Key1", rand1.toString())
+            startActivity(intent)
+       }
+        tvNumber1.text = intent.getStringExtra("Key2")
 
     }}
